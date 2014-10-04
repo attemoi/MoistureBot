a4757910151df828db1ce8d451e23c2a17b5e7f5
Atte Moisio <atte.moisio@attemoisio.fi>
2014-10-04 20:07:03 +0300

    Fix bugs and refactor

--- moisture-bot-lib/ChatRoomMessage.cs
+++ moisture-bot-lib/ChatRoomMessage.cs
@@ -2,15 +2,13 @@
 
 namespace moisturebot
 {
-	public class ChatRoomMessage : ChatMessage
+	public class ChatMessage
 	{
 		public string Message { get; internal set; }
 		public ulong ChatterId { get; internal set; }
 		public ulong ChatId { get; internal set; }
-		public ChatRoomMessage(string message, ulong chatterId, ulong chatId)
+		public ChatMessage(string message, ulong chatterId, ulong chatId)
 		{
-			super (message, chatterId);
-
 			this.Message = message;
 			this.ChatterId = chatterId;
 			this.ChatId = chatId;
