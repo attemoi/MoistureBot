ac71672ce0d4ce1f10cfff0dd5fcaaa546b26aa5
Atte Moisio <atte.moisio@attemoisio.fi>
2014-10-15 23:31:50 +0300

    Refactor and add url logging to SQLiteChatLogger

--- SampleAddins/SQLiteChatLogger/SQLiteChatLogger.cs
+++ SampleAddins/SQLiteChatLogger/SQLiteChatLogger.cs
@@ -7,6 +7,7 @@ using Mono.Addins;
 using Mono.Data.Sqlite;
 using System.Data;
 using System.IO;
+using System.Collections.Generic;
 
 [assembly:Addin("SQLiteChatLogger", "1.0")]
 [assembly:AddinDependency("MoistureBot", "1.0")]
@@ -23,6 +24,8 @@ namespace MoistureBot
 	{
 		const string CONFIG_SECTION = "sqlite_chat_logger";
 		const string CONFIG_KEY = "database_path";
+
+		const string URL_REGEX = @"\b(?:https?://|www\.)\S+\b";
 
 		string connectionString;
 
@@ -64,131 +67,165 @@ namespace MoistureBot
 
 			Logger.Info("Creating tables...");
 
-			IDbConnection conn = null;
-			IDbCommand cmd = null;
 			try 
 			{
-			
-				var sql = @"
+				using (var conn = (IDbConnection)new SqliteConnection(connectionString)) 
+				using (var cmd = conn.CreateCommand())
+				{
+					var sql = @"
 
-					CREATE TABLE IF NOT EXISTS friend_chat (
-					timestamp TEXT NOT NULL,
-					user_id TEXT NOT NULL,
-					message TEXT NOT NULL
-					);
+						CREATE TABLE IF NOT EXISTS friend_chat (
+							timestamp TEXT NOT NULL,
+							user_id TEXT NOT NULL,
+							message TEXT NOT NULL
+						);
 
-					CREATE TABLE IF NOT EXISTS group_chat (
-					timestamp TEXT NOT NULL,
-					user_id TEXT NOT NULL,
-					room_id TEXT NOT NULL,
-					message TEXT NOT NULL
-					);
+						CREATE TABLE IF NOT EXISTS group_chat (
+							timestamp TEXT NOT NULL,
+							user_id TEXT NOT NULL,
+							room_id TEXT NOT NULL,
+							message TEXT NOT NULL
+						);
 
-				";
+						CREATE TABLE IF NOT EXISTS group_chat_urls (
+							timestamp TEXT NOT NULL,
+							user_id TEXT NOT NULL,
+							room_id TEXT NOT NULL,
+							url TEXT NOT NULL
+						);
 
-				conn = (IDbConnection) new SqliteConnection(connectionString);
-				conn.Open();
+						CREATE TABLE IF NOT EXISTS friend_chat_urls (
+							timestamp TEXT NOT NULL,
+							user_id TEXT NOT NULL,
+							url TEXT NOT NULL
+						);
+					";
 
-				cmd = conn.CreateCommand();
-				cmd.CommandType = CommandType.Text;
-				cmd.CommandText = sql;
-				cmd.ExecuteNonQuery();
+					conn.Open();
 
-				Logger.Info("Log tables created");
+					cmd.CommandType = CommandType.Text;
+					cmd.CommandText = sql;
+					cmd.ExecuteNonQuery();
+
+					Logger.Info("Log tables created");
+				}
 
 			} 
 			catch(Exception e)
 			{
 				Logger.Error("Failed to create tables",e);
 			}
-			finally
-			{
-				if (cmd != null)
-				{
-					cmd.Dispose();
-					cmd = null;
-				}
-				if (conn != null)
-				{
-					conn.Close();
-					conn = null;
-				}
-			}
+			
 		}
 			
 		public void MessageReceived(FriendChatMessage message)
 		{
-			IDbConnection conn = null;
-			IDbCommand cmd = null;
-			try 
-			{
-				conn = (IDbConnection) new SqliteConnection(connectionString);
-				conn.Open();
-			
-				var sql = "INSERT INTO friend_chat (timestamp, user_id, message)" +
-				          " VALUES (@timestamp, @user_id, @message)";
-				cmd = conn.CreateCommand();
-				cmd.CommandType = CommandType.Text;
-				cmd.CommandText = sql;
+			Logger.Info("Logging friend chat message to SQLite");
+		
+			var sql = "INSERT INTO friend_chat (timestamp, user_id, message)" +
+			          " VALUES (@timestamp, @user_id, @message)";
 
-				cmd.Parameters.Add(new SqliteParameter("@timestamp", DateTimeSQLite(DateTime.Now)));
-				cmd.Parameters.Add(new SqliteParameter("@message", message.Message));
-				cmd.Parameters.Add(new SqliteParameter("@user_id", message.ChatterId));
+			List<SqliteParameter> parameters = new List<SqliteParameter>();
+			var time = DateTimeSQLite(DateTime.Now);
+			parameters.Add(new SqliteParameter("@timestamp", time));
+			parameters.Add(new SqliteParameter("@message", message.Message));
+			parameters.Add(new SqliteParameter("@user_id", message.ChatterId));
 
-				cmd.ExecuteNonQuery();
-			}
-			catch(Exception e)
+			ExecuteQuery(sql,parameters);
+
+			// Check if message contains urls
+			MatchCollection matches = Regex.Matches(message.Message,URL_REGEX);
+			if (matches == null)
+				return;
+
+			foreach (Match m in matches)
 			{
-				Logger.Error("Failed to log friend chat message to sqlite database",e);
+				Logger.Info("Found url in message, inserting to database");
+
+				var urlSql = "INSERT INTO friend_chat_urls (timestamp, user_id, url)" +
+				             " VALUES (@timestamp, @user_id, @url)";
+
+				var urlParameters = new List<SqliteParameter>();
+
+				urlParameters.Add(new SqliteParameter("@timestamp", time));
+				urlParameters.Add(new SqliteParameter("@user_id", message.ChatterId));
+				urlParameters.Add(new SqliteParameter("@url", m.Value));
+
+				ExecuteQuery(urlSql,urlParameters);
 			}
-			finally
-			{
-				if (cmd != null)
-				{
-					cmd.Dispose();
-					cmd = null;
-				}
-				if (conn != null)
-				{
-					conn.Close();
-					conn = null;
-				}
-			}
+		
+		
 		}
 			
 		public void MessageReceived(GroupChatMessage message)
 		{
-			IDbConnection conn = null;
 
-			try 
+			Logger.Info("Logging group chat message to SQLite");
+
+			var sql = "INSERT INTO group_chat (timestamp, user_id, room_id, message)" +
+				" VALUES (@timestamp, @user_id, @room_id, @message)";
+
+			var parameters = new List<SqliteParameter>();
+			var time = DateTimeSQLite(DateTime.Now);
+			parameters.Add(new SqliteParameter("@timestamp", time));
+			parameters.Add(new SqliteParameter("@user_id", message.ChatterId));
+			parameters.Add(new SqliteParameter("@room_id", message.ChatId ));
+			parameters.Add(new SqliteParameter("@message", message.Message));
+
+			ExecuteQuery(sql,parameters);
+
+			// Check if message contains urls
+			MatchCollection matches = Regex.Matches(message.Message,URL_REGEX);
+			if (matches == null)
+				return;
+
+			foreach (Match m in matches)
 			{
-				conn = (IDbConnection)new SqliteConnection(connectionString);
-				conn.Open();
+				Logger.Info("Found url in message, inserting to database");
 
-				var sql = "INSERT INTO group_chat (timestamp, user_id, room_id, message)" +
-					" VALUES (@timestamp, @user_id, @room_id, @message)";
-				var cmd = conn.CreateCommand();
-				cmd.CommandType = CommandType.Text;
-				cmd.CommandText = sql;
+				var urlSql = "INSERT INTO group_chat_urls (timestamp, user_id, room_id, url)" +
+				             " VALUES (@timestamp, @user_id, @room_id, @url)";
 
-				cmd.Parameters.Add(new SqliteParameter("@timestamp", DateTimeSQLite(DateTime.Now)));
-				cmd.Parameters.Add(new SqliteParameter("@message", message.Message));
-				cmd.Parameters.Add(new SqliteParameter("@user_id", message.ChatterId));
-				cmd.Parameters.Add(new SqliteParameter("@room_id", message.ChatId ));
+				var urlParameters = new List<SqliteParameter>();
 
-				cmd.ExecuteNonQuery();
+				urlParameters.Add(new SqliteParameter("@timestamp", time));
+				urlParameters.Add(new SqliteParameter("@user_id", message.ChatterId));
+				urlParameters.Add(new SqliteParameter("@room_id", message.ChatId));
+				urlParameters.Add(new SqliteParameter("@url", m.Value));
+
+				ExecuteQuery(urlSql,urlParameters);
+			}
+
+
+		}
+
+		private void ExecuteQuery(string sql, IEnumerable<SqliteParameter> parameters) {
+
+			try  
+			{
+				using (var conn = (IDbConnection)new SqliteConnection(connectionString)) 
+				using (var cmd = conn.CreateCommand())
+				{
+					conn.Open();
+					cmd.CommandType = CommandType.Text;
+					cmd.CommandText = sql;
+
+					foreach (SqliteParameter p in parameters) 
+					{
+						cmd.Parameters.Add(p);
+					}
+
+					cmd.ExecuteNonQuery();
+				}
 			}
 			catch(Exception e)
 			{
-				Logger.Error("Failed to log group chat message to sqlite database",e);
+				Logger.Error("Failed to log chat message to sqlite database",e);
 			}
-			finally
-			{
-				if (conn != null)
-					conn.Close();
-			}
-
+		
 		}
+
+
 
 		private string DateTimeSQLite(DateTime datetime)
 		{
