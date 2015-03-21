d40022d3a303ca169a89b54c597f229535b0f333
Atte Moisio <atte.moisio@attemoisio.fi>
2015-03-21 13:17:19 +0200

    Improve Moikkaaja addon performance using Dictionary

--- Addins/Moikkaaja/Moikkaaja/Moikkaaja.cs
+++ Addins/Moikkaaja/Moikkaaja/Moikkaaja.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using MoistureBot.ExtensionPoints;
 using MoistureBot.Steam;
 using Mono.Addins;
+using System.Collections.Generic;
 
 [assembly:Addin("Moikkaaja", "1.0")]
 [assembly:AddinDependency("MoistureBot", "1.0")]
@@ -22,6 +23,35 @@ namespace MoistureBot
 		private IMoistureBot Bot = MoistureBotComponentProvider.GetBot();
 		private ILogger Logger = MoistureBotComponentProvider.GetLogger();
 
+		Dictionary<String, String> replyDict;
+
+		public Moikkaaja() {
+
+			string[] greetings = {
+				"Moikka taas", "Moikkelis", "Moi kaikki", "Moikkamoi", 
+				"Moikka", "Moips", "Moiks", "Moik", "Moi", "Terve", 
+				"Hello", "Tsau", "Heippa", "Heips", "Hei",
+				"Hoi", "Hola", "Iltaa", "Päivää", "Huomenta", "Moro",
+				"Hyvää yötä", "Hyvää päivää", "Hyvää huomenta", "Hyvää iltaa", 
+				"Näkemiin", "Hei hei", "Moikka moi"
+			};
+
+			// Convert the array to a dictionary with a stripped lowercase string as the key.
+			replyDict = greetings
+				.Select((str) => new { 
+					Value = str, 
+					Key = stripMessage(str)
+				})
+				.ToDictionary(
+					x => x.Key,
+					x => x.Value
+				);
+		}
+
+		private string stripMessage(String value) {
+			return new Regex("[^a-zäöA-ZÄÖ0-9\\s-]").Replace(value,"").ToLower();
+		}
+
 		public void MessageReceived(GroupChatMessage message)
 		{
 			var reply = CreateReply(message);
@@ -38,26 +68,21 @@ namespace MoistureBot
 
 		string CreateReply(FriendChatMessage message)
 		{
-			string[] greetings = {
-				"Moikka taas", "Moikkelis", "Moi kaikki", "Moikkamoi", 
-				"Moikka", "Moips", "Moiks", "Moik", "Moi", "Terve", 
-				"Hello", "Tsau", "Heippa", "Heips", "Hei",
-				"Hoi", "Hola", "Iltaa", "Päivää", "Huomenta", "Moro",
-				"Hyvää yötä", "Hyvää päivää", "Hyvää huomenta", "Hyvää iltaa", 
-				"Näkemiin", "Hei hei", "Moikka moi"
-			};
-
-			Regex rgx = new Regex("[^a-zäöA-ZÄÖ0-9 -]");
-			string strippedMsg = rgx.Replace(message.Message,"").ToLower();
-			string msg = Array.Find(greetings,t => t.Equals(strippedMsg,StringComparison.InvariantCultureIgnoreCase));
-
-			if (msg != null && !msg.Equals(""))
+			string key = stripMessage(message.Message);
+			string reply;
+			if (replyDict.TryGetValue(key, out reply))
 			{
 				Logger.Info("Moikkaaja: Greeting received, replying");
-				msg += " " + Bot.GetPersonaName(message.ChatterId) + "!";
+				reply += " " + Bot.GetPersonaName(message.ChatterId) + "!";
+				return reply;
+			}
+			else 
+			{
+				return null;
 			}
 
-			return msg;
+
+
 		}
 	}
 }
