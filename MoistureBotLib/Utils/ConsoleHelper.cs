c7febcfdca935439278a1bc3dee9e54abeb43769
Atte Moisio <atte.moisio@attemoisio.fi>
2015-03-25 15:10:03 +0200

    Fix Xamarin studio build

--- MoistureBotLib/Utils/ConsoleHelper.cs
+++ MoistureBotLib/Utils/ConsoleHelper.cs
@@ -12,16 +12,22 @@ namespace MoistureBot.Utils
         /// </summary>
         /// <param name="mask">a <c>char</c> representing your choice of console mask</param>
         /// <returns>the string the user typed in </returns>
+
+        /// <summary>
+        /// Like System.Console.ReadLine(), only with a mask.
+        /// </summary>
+        /// <param name="mask">a <c>char</c> representing your choice of console mask</param>
+        /// <returns>the string the user typed in </returns>
         public static string ReadPassword(char mask)
         {
-            const int ENTER = 13, ENTER2 = 33, BACKSP= 8 , CTRLBACKSP= 127;
-            int[] FILTERED = { 0, 27, 9, 10 /*, 32 space, if you care */ }; // const
+
+            ConsoleKey[] FILTERED = { ConsoleKey.Escape, ConsoleKey.Tab }; // const
 
             var pass = new Stack<char>();
-            char chr = (char)0;
-            while ((chr = System.Console.ReadKey(true).KeyChar) != ENTER && chr != ENTER2)
+            ConsoleKeyInfo key;
+            while ((key = System.Console.ReadKey(true)).Key != ConsoleKey.Enter)
             {
-                if (chr == BACKSP)
+                if (key.Key.Equals(ConsoleKey.Backspace))
                 {
                     if (pass.Count > 0)
                     {
@@ -29,20 +35,12 @@ namespace MoistureBot.Utils
                         pass.Pop();
                     }
                 }
-                else if (chr == CTRLBACKSP)
-                {
-                    while (pass.Count > 0)
-                    {
-                        System.Console.Write("\b \b");
-                        pass.Pop();
-                    }
-                }
-                else if (FILTERED.Count(x => chr == x) > 0)
+                else if (FILTERED.Count(x => key.Key == x) > 0)
                 {
                 }
                 else
                 {
-                    pass.Push((char)chr);
+                    pass.Push(key.KeyChar);
                     System.Console.Write(mask);
                 }
             }
